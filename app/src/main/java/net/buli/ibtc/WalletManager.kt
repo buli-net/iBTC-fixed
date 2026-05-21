648839e83c4bc6832bb1f51bff14195a43076736
buli-net <hvlrcb@gmail.com>
2026-05-21 23:12:13 +0700

    Update WalletManager.kt

--- app/src/main/java/net/buli/ibtc/WalletManager.kt
+++ app/src/main/java/net/buli/ibtc/WalletManager.kt
@@ -7,6 +7,7 @@ import org.bitcoinj.params.MainNetParams
 import org.bitcoinj.wallet.DeterministicSeed
 import org.bitcoinj.wallet.Wallet
 import java.io.File
+import java.security.SecureRandom
 
 object WalletManager {
     private val params: NetworkParameters = MainNetParams.get()
@@ -15,11 +16,12 @@ object WalletManager {
     fun hasWallet(c:Context)=walletFile(c).exists()
 
     fun createWallet(c:Context):List<String>{
-        val seed = DeterministicSeed(MnemonicCode.INSTANCE.generateMnemonic(),null,"",System.currentTimeMillis()/1000)
+        val seed = DeterministicSeed(SecureRandom(), 128, "", System.currentTimeMillis()/1000)
         val w = Wallet.fromSeed(params, seed)
         w.saveToFile(walletFile(c))
         return seed.mnemonicCode!!
     }
+    
     fun importWallet(c:Context, words:String):Boolean{
         return try{
             val seed = DeterministicSeed(words.trim().split(" "),null,"",0L)
@@ -27,6 +29,7 @@ object WalletManager {
             w.saveToFile(walletFile(c)); true
         }catch(e:Exception){false}
     }
+    
     fun getAddress(c:Context):String{
         val w = Wallet.loadFromFile(walletFile(c))
         return w.currentReceiveAddress().toString()
