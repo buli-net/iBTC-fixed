3fa72dc5be7d8fd05f9d12971a054e2dc08fc2a4
buli-net <hvlrcb@gmail.com>
2026-05-23 11:56:07 +0700

    Update SyncWorker.kt

--- app/src/main/java/net/buli/ibtc/SyncWorker.kt
+++ app/src/main/java/net/buli/ibtc/SyncWorker.kt
@@ -1,14 +1,6 @@
 package net.buli.ibtc
-
 import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-
-class SyncWorker(ctx:Context, params:WorkerParameters): CoroutineWorker(ctx, params){
-    override suspend fun doWork(): Result = withContext(Dispatchers.IO){
-        try{ val wm = WalletManager(applicationContext); wm.init(); wm.price(); wm.getBalance(); Result.success() }
-        catch(e:Exception){ Result.retry() }
-    }
-}
+import kotlinx.coroutines
