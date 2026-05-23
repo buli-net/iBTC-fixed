d64b5f1a6e57010c171afc0ba53ac16e7f799e74
buli-net <hvlrcb@gmail.com>
2026-05-23 12:10:50 +0700

    Delete SyncWorker.kt

--- app/src/main/java/net/buli/ibtc/SyncWorker.kt
+++
@@ -1,6 +0,0 @@
-package net.buli.ibtc
-import android.content.Context
-import androidx.work.CoroutineWorker
-import androidx.work.WorkerParameters
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines
