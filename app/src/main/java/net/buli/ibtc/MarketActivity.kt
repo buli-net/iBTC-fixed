43813c1e23e55c77bbca6e812232972f088b5ed9
buli-net <hvlrcb@gmail.com>
2026-05-24 12:05:36 +0700

    Update MarketActivity.kt

--- app/src/main/java/net/buli/ibtc/MarketActivity.kt
+++ app/src/main/java/net/buli/ibtc/MarketActivity.kt
@@ -1,8 +1,10 @@
 package net.buli.ibtc
 import android.os.Bundle
+
 class MarketActivity : BaseNavActivity() {
     override fun onCreate(b: Bundle?) {
-        super.onCreate(b); setContentView(R.layout.activity_market)
+        super.onCreate(b)
+        setContentView(R.layout.activity_market)
         setupNav(R.id.navMarket)
     }
 }
