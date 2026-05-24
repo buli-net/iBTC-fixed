2a9a182546fcbe997c3372c8db5330bc1cc00f9a
buli-net <hvlrcb@gmail.com>
2026-05-24 16:55:23 +0700

    Update LockActivity.kt

--- app/src/main/java/net/buli/ibtc/LockActivity.kt
+++ app/src/main/java/net/buli/ibtc/LockActivity.kt
@@ -9,6 +9,7 @@ class LockActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lock)
+        
         val sec = SecurePrefs(this)
         val etPassword = findViewById<EditText>(R.id.etPassword)
         val btnUnlock = findViewById<Button>(R.id.btnUnlock)
@@ -17,7 +18,7 @@ class LockActivity : BaseActivity() {
             if (sec.checkPwd(etPassword.text.toString())) {
                 getSharedPreferences("ibtc_prefs",0).edit().putBoolean("locked",false).apply()
                 startActivity(Intent(this, MainActivity::class.java)); finish()
-            } else { Toast.makeText(this,"Sai MK",Toast.LENGTH_SHORT).show() }
+            } else Toast.makeText(this,"Sai MK",Toast.LENGTH_SHORT).show()
         }
     }
     override fun onBackPressed() {}
